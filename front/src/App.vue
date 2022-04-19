1b842da2d51116ab229aa85839842544cb7bb26b
LKH <mogies@naver.com>
2022-04-19 15:21:56 +0900

    FE: Feat: 로그인 페이지 레이아웃 작성

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <nav>
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    </nav> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
 }
 
 nav {
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -10,7 +10,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/StartView.vue')
+    component: () => import('../views/Start.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
