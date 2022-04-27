97f539742384c859ecd9180093a70de38cbbed16
이정현 <simba155korea@gmail.com>
2022-04-27 16:09:14 +0900

    feat : 캐릭터 관련 기능 db

--- back/src/main/java/com/daki/db/repository/DollRepository.java
+++ back/src/main/java/com/daki/db/repository/DollRepository.java
@@ -1,8 +1,14 @@
 package com.daki.db.repository;
 
 import com.daki.db.entity.Doll;
+import com.daki.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DollRepository extends JpaRepository<Doll, Long> {
+//    @Override
+//    Doll getById(Long aLong);
 
+    Doll findByUser(User user);
 }
