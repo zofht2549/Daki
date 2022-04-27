351c4ba243d67bfd5a482d66dfed6ff09e5be18b
이정현 <simba155korea@gmail.com>
2022-04-27 16:08:16 +0900

    feat : 캐릭터 관련 기능 Service

--- back/src/main/java/com/daki/api/service/DollService.java
+++ back/src/main/java/com/daki/api/service/DollService.java
@@ -1,13 +1,15 @@
 package com.daki.api.service;
 
+import com.daki.api.request.DollCreateReq;
+import com.daki.api.request.DollLikeableUpdateReq;
+import com.daki.api.request.DollUpdateReq;
+import com.daki.api.response.*;
 
+public interface DollService {
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class DollService {
-
-
-
+    DollReadRes readDollInfo(Long dollNo);
+    DollUpdateRes updateDollInfo(DollUpdateReq dollUpdateReq);
+    DollDeleteRes deleteDoll(Long dollNo);
+    DollLikeableUpdateRes updateDollLikeable(DollLikeableUpdateReq dollLikeableUpdateReq);
 
 }
