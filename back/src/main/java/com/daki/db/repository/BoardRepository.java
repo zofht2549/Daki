37e413855dfe4d7e8eb6c46a3bce03a167702e11
protossking <upgrademarine5@icloud.com>
2022-04-30 23:55:57 +0900

    [Be]Feat: 댓글 등록 Rest api

--- back/src/main/java/com/daki/db/repository/BoardRepository.java
+++ back/src/main/java/com/daki/db/repository/BoardRepository.java
@@ -2,7 +2,9 @@ package com.daki.db.repository;
 
 import com.daki.db.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
 }
